d1f8f8bd323a99b22292d54a1e46e645cb6820c9
Allan Wilson <Jockey@allans-macbook-air.local>
2015-10-02 14:42:59 -0700

    Initial Commit

---
+++ LastTry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LastTry
+//
+//  Created by Allan Wilson on 2015-10-02.
+//  Copyright © 2015 Wilson Bros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
